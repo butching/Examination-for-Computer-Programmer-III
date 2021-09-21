a6a60598d31d11d08009678b83b80ce1c23d848d
butching <butching@ymail.com>
2021-09-21 10:20:44 +0800

    Create excercise3.cs
    3. Write a C# program to rotate an array (length 3) of integers in left direction:
    e.g.
     Enter a number: 543
     Output: 345

---
+++ excercise-3/excercise3.cs
@@ -0,0 +1,20 @@
+using System;  
+public class ExerciseThree  
+{  
+  public static void Main(string[] args)  
+  {  
+    int  n, reverse=0, rem;           
+    Console.Write("Enter a number: ");      
+    n= int.Parse(Console.ReadLine());     
+		if (n > 999 || n < 100) {
+      throw new InvalidOperationException("Number must be 3 DIGIT only");
+    } 
+    while(n!=0)      
+    {      
+      rem=n%10;        
+      reverse=reverse*10+rem;      
+      n/=10;      
+    }      
+    Console.Write("Reversed Number: "+reverse);       
+  }  
+}  
